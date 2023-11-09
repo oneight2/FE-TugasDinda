ab569f5a486e6cf72d97ca533d36ca5143b2dab6
M Syarif Hidayat <syarif.hidayat@biofarma.co.id>
2023-11-09 07:30:46 +0700

    update config

--- includes/configg.php
+++ includes/configg.php
@@ -1,11 +1,10 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "bihypetherap";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "bihypetherapy";
 
-    $connection = mysqli_connect($servername, $username, $password, $dbname);
-    if (!$connection)
-    {
-        die("Connextion Failed : ".mysqli_connect_error());
-    }
+$connection = mysqli_connect($servername, $username, $password, $dbname);
+if (!$connection) {
+    die("Connextion Failed : " . mysqli_connect_error());
+}
